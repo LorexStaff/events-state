d788cba6178e23dd6a4e6f29599b2feb84a7eeb8
Kristina Shakhvorostova <kristinashah2512@gmail.com>
2025-12-07 20:25:09 +0300

    fixed

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,29 +1,19 @@
 import React from "react";
 
 const ProjectList = ({ projects }) => {
-  const col0 = [];
-  const col1 = [];
-  const col2 = [];
-
-  projects.forEach((project, index) => {
-    if (index % 3 === 0) col0.push(project);
-    else if (index % 3 === 1) col1.push(project);
-    else col2.push(project);
-  });
-
   return (
     <div className="project-columns">
-      <div className="project-column">{col0.map(renderProject)}</div>
-      <div className="project-column">{col1.map(renderProject)}</div>
-      <div className="project-column">{col2.map(renderProject)}</div>
+      {projects.map((project) => (
+        <div key={project.id} className="project-item">
+          <img
+            src={project.imageUrl.trim()}
+            alt={project.title}
+            loading="lazy"
+          />
+        </div>
+      ))}
     </div>
   );
 };
-
-const renderProject = (project) => (
-  <div key={project.id} className="project-item">
-    <img src={project.imageUrl.trim()} alt={project.title} loading="lazy" />
-  </div>
-);
 
 export default ProjectList;
